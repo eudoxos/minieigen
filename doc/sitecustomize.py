ebb3ba7865e70bad0309a810de234eb7cf3c4e39
eudoxos <eu@doxos.eu>
2015-04-13 14:20:19 +0200

    Generate docs

--- doc/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-# unf-8 in docstring (or where else) makes sphinx fail; force utf-8 encoding this way, run with PYTHONPATH=.
-import sys
-sys.setdefaultencoding('utf-8')
--- src/expose.hpp
+++
@@ -1,7 +0,0 @@
-// functions defined in the respective .cpp files
-void expose_matrices();
-void expose_vectors();
-void expose_boxes();
-void expose_quaternion();
-void expose_complex(); // does nothing if _COMPLEX_SUPPORT is not #defined
-void expose_converters();
